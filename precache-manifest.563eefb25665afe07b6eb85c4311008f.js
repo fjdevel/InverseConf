4c3a679fe6dba6bfb73090b73135a67d795e7dec
Frederick Mejia <fjdevel@gmail.com>
2019-11-13 15:14:10 -0600

    Updates

---
+++ precache-manifest.563eefb25665afe07b6eb85c4311008f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c269418c36a5bd171f545c70d84b668",
+    "url": "/InverseConff/index.html"
+  },
+  {
+    "revision": "de5aed0b7b84cc0a42ca",
+    "url": "/InverseConff/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "619263150cc2d914604b",
+    "url": "/InverseConff/static/js/2.621283f4.chunk.js"
+  },
+  {
+    "revision": "de5aed0b7b84cc0a42ca",
+    "url": "/InverseConff/static/js/main.a77f4d69.chunk.js"
+  },
+  {
+    "revision": "bdd271c3280c8cb6f2e5",
+    "url": "/InverseConff/static/js/runtime-main.a42d3e2a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/InverseConff/static/media/logo.25bf045c.svg"
+  }
+]);
